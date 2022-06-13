08df3871ae254989a318f3fb530c0f2123236304
@dmin <e.amaraa.3@live.com>
2022-06-13 10:04:59 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log('request event');
-    res.end('Hello World');
+    req.url === '/' ? res.end('Home Page') : req.url === '/about' ? res.end('About Page') : res.end('Error Page');
 });
 server.listen(5000, () => console.log('Server listening on port : 5000....'));
