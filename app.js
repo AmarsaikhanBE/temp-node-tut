1303eb81650d5f0d5fc1632ae79d43b0aaa75f97
@dmin <e.amaraa.3@live.com>
2022-06-16 21:42:17 +0800

    async-patterns

--- app.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    req.url === '/' ? res.end('Home Page') : req.url === '/about' ? res.end('About Page') : res.end('Error Page');
-});
-server.listen(5000, () => console.log('Server listening on port : 5000....'));
