da642b4d55d8b1bfe5dc55960bd6f93661b18178
@dmin <e.amaraa.3@live.com>
2022-06-17 21:24:35 +0800

    update

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -4,21 +4,23 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err);
         return;
-    };
-    const first = result;
-    readFile('./content/second.txt', 'utf8', (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        };
-        const second = result;
-        writeFile('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, (err, result) => {
+    } else {
+        const first = result;
+        readFile('./content/second.txt', 'utf8', (err, result) => {
             if (err) {
                 console.log(err);
                 return;
+            } else {
+                const second = result;
+                writeFile('./content/result-async.txt', `Here is the result : ${first}, ${second}`, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    };
+                    console.log('done with this task');
+                });
             };
-            console.log('done with this task');
         });
-    });
+    };
 });
 console.log('starting next task');
--- 17-http-stream.js
+++ 17-http-stream.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 http.createServer(function(req, res) {
     // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
